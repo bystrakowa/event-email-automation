c69e5a9a8a4bea9d6fbd9aac00bc3044ee13f027
bystrakowa <84568820+bystrakowa@users.noreply.github.com>
2026-02-11 16:44:19 +0100

    Bump version to 0.2.0 and add dateutil dependency

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="event_emailer",
-    version="0.1.1",
+    version="0.2.0",
     packages=find_packages(),
-    install_requires=[
-        "flexus-client-kit",
-        "google-auth",
-        "google-auth-oauthlib",
-        "google-auth-httplib2",
-        "google-api-python-client",
-        "motor",
-        "pymongo",
-        "pytest-asyncio",
-    ],
+ install_requires=[
+    "flexus-client-kit",
+    "google-auth",
+    "google-auth-oauthlib",
+    "google-auth-httplib2",
+    "google-api-python-client",
+    "motor",
+    "pymongo",
+    "pytest-asyncio",
+    "python-dateutil",
+]
     package_data={"": ["*.webp", "*.png", "*.html", "*.lark", "*.json"]},
 )
